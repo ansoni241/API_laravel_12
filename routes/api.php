e7018347a5ebd73de949663730d441012bf9d936
Anthony Gomez <anthonyansoni24@gmail.com>
2025-08-11 22:01:24 -0400

    Se agrego las rutas de los controladores

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\CompraController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::apiResource('marcas', MarcaController::class);
+Route::apiResource('categorias', CategoriaController::class);
+Route::apiResource('productos', ProductoController::class);
+Route::apiResource('compras', CompraController::class);
+
+Route::get('categorias/{categoria}/productos', [CategoriaController::class, 'productosPorCategoria']);
+Route::get('marcas/{marca}/productos', [MarcaController::class, 'productosPorMarca']);
